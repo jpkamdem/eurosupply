be65991e7b4a3dcdac8749a78c11861c0c9f00da
jpkamdem <jpkamdem@proton.me>
2026-09-22 18:28:54 +0200

    correctif déploiement

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker compose down -v
 docker rmi eurosupply/spring:0.5 eurosupply/angular:0.5
-docker compose build --no-cache
+docker build --no-cache --file Dockerfile --target angular --tag eurosupply/angular:0.5 .
+docker build --no-cache --file Dockerfile --target spring --tag eurosupply/spring:0.5 .
 docker compose up --wait
